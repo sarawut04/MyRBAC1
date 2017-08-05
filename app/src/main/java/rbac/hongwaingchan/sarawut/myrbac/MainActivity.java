24da7c9c15839dc45065d2cb88662afcf324b6aa
sarawut <sarawut.57011@gmail.com>
2017-08-05 14:47:06 +0700

    5ags

--- app/src/main/java/rbac/hongwaingchan/sarawut/myrbac/MainActivity.java
+++ app/src/main/java/rbac/hongwaingchan/sarawut/myrbac/MainActivity.java
@@ -3,11 +3,24 @@ package rbac.hongwaingchan.sarawut.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rbac.hongwaingchan.sarawut.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+                //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContrainer, mainFragment)
+                    .commit();
+        }
+
+    }       //Main Method
+
+}       //Main Class
