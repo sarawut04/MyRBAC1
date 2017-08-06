504f8f64638eac553dfc4ce5cd45eb385cc670ac
Administrator <sarawut.57011@gmail.com>
2017-08-06 12:12:22 +0700

    .0.

--- app/src/main/java/rbac/hongwaingchan/sarawut/myrbac/fragment/MainFragment.java
+++ app/src/main/java/rbac/hongwaingchan/sarawut/myrbac/fragment/MainFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import rbac.hongwaingchan.sarawut.myrbac.R;
+import rbac.hongwaingchan.sarawut.myrbac.SignUpFragment;
 
 /**
  * Created by Administrator on 5/8/2560.
@@ -20,6 +22,36 @@ public class MainFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.main_fragment, container, false);
         return view;
+
+    }   //onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        //Register Controller
+        registerController();
+
+
+    }
+
+    private void registerController() {
+        TextView textView = (TextView) getView().findViewById(R.id.txtNewRegister);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SignUpFragment signUpFragment = new SignUpFragment();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainContrainer, signUpFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
 
     }
 } // Main Class
